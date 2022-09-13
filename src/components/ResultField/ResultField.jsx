9063eab163c71579fab414d0c621835b8928954e
Kevin <jung.kevinj@gmail.com>
2022-09-13 13:35:54 -0700

    update: display API response onto app after fetching. CSS complete for design, mobile only

--- src/components/ResultField/ResultField.jsx
+++ src/components/ResultField/ResultField.jsx
@@ -1,15 +1,28 @@
 import React from "react";
 import "./ResultField.css";
-import Field from "../Field/Field";
 
 const ResultField = ({ data }) => {
-  const transformData = () => {
-    for (let key in data) {
-      <Field name={key} value={data[key]} />;
-    }
-  };
-
-  return <div>{transformData()}</div>;
+  const { ip, location, timezone, isp } = data;
+  return (
+    <div className="results-container">
+      <div className="field">
+        <h3>ip address</h3>
+        <p>{ip}</p>
+      </div>
+      <div className="field">
+        <h3>location</h3>
+        <p>{location}</p>
+      </div>
+      <div className="field">
+        <h3>timezone</h3>
+        <p>{`UTC ${timezone}`}</p>
+      </div>
+      <div className="field">
+        <h3>isp</h3>
+        <p>{isp}</p>
+      </div>
+    </div>
+  );
 };
 
 export default ResultField;
