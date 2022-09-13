ed19648a9c2b60e344f4bb83be2706b2b32a41a9
Kevin <jung.kevinj@gmail.com>
2022-09-13 13:37:19 -0700

    update: initiate api fetch when input btn clicked. Fix handler function onClick

--- src/components/Searchbar/Searchbar.jsx
+++ src/components/Searchbar/Searchbar.jsx
@@ -1,11 +1,26 @@
 import arrowImg from "../../images/icon-arrow.svg";
 import "./Searchbar.css";
+import { useState } from "react";
 
-const Searchbar = () => {
+const Searchbar = ({ fetchApi }) => {
+  const [searchField, setSearchField] = useState("");
+
+  const handleClick = () => {
+    console.log(searchField);
+    // Need to do some validation to make sure it is an IP address or domain (later)
+    // Fetch API using IP address to get response
+    fetchApi(searchField);
+  };
+
   return (
     <div className="searchbar">
-      <input type="text" placeholder="Search for any IP address or domain" />
-      <button className="searchbar-btn">
+      <input
+        type="text"
+        placeholder="Search for any IP address or domain"
+        onChange={(e) => setSearchField(e.target.value)}
+        value={searchField}
+      />
+      <button className="searchbar-btn" onClick={handleClick}>
         <img src={arrowImg} />
       </button>
     </div>
